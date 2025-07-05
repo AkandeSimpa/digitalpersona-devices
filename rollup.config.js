e02a28aa98914f115181115eb6d665e4ba4d867f
AkandeSimpa <138795002+AkandeSimpa@users.noreply.github.com>
2025-07-05 01:09:32 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,29 @@
-// This configuration expects environment parameters passed to the rollup:
-// (e.g. "rollup -c rollup.config.js --environment target:es5,format:umd,minify",
 const {
-    minify,     // true: minify the bundle, false|undefined: do not minify the bundle (default)
-    target,     // target syntax (es5, es6, ...). Default: es5
-    format,      // bundle format (umd, cjs, ...). Default: umd
-    npm_package_globalObject,
-} = {
-    target: "es5",
-    format: "umd",
-    ...process.env
-}
+  minify,
+  target = "es5",
+  format = "umd",
+  npm_package_globalObject,
+} = process.env;
 
-import node from 'rollup-plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+const outputName = npm_package_globalObject || "dp.devices";
+
+import node from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 export default {
-    input: `dist/${target}/index.js`,
-    external: ['@digitalpersona/core', '@digitalpersona/services', 'WebSdk'],
-    output: {
-        format,
-        extend: true,
-        name: npm_package_globalObject,
-        globals: {
-            '@digitalpersona/core': 'dp.core',
-            '@digitalpersona/services': 'dp.services',
-            'WebSdk': 'WebSdk',
-          },
-        file: `dist/${target}.bundles/index.${format}${minify ? '.min' : ''}.js`,
-        sourcemap: true
+  input: `dist/${target}/index.js`,
+  external: ["@digitalpersona/core", "@digitalpersona/services", "WebSdk"],
+  output: {
+    format,
+    extend: true,
+    name: outputName,
+    globals: {
+      "@digitalpersona/core": "dp.core",
+      "@digitalpersona/services": "dp.services",
+      WebSdk: "WebSdk",
     },
-    plugins: [
-        node(),
-        (minify ? terser() : [])
-    ]
-}
+    file: `dist/${target}.bundles/index.${format}${minify ? ".min" : ""}.js`,
+    sourcemap: true,
+  },
+  plugins: [node(), ...(minify ? [terser()] : [])],
+};
